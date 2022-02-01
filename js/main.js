e9d507f41cd95ab1f0e2ee8e09751cf201375d23
Aceskicker <moralesyesid07@gmail.com>
2022-02-01 17:05:26 -0500

    agrego la barra lateral de los trailers desde la mediaquery de tablet utilizando jquery manteniendo el contenido en forma responsive

--- js/main.js
+++ js/main.js
@@ -3,4 +3,10 @@ $(document).ready(function(){
         $("#burguer").toggleClass("abierto");
         $(".section-header").toggleClass("abierto");
     })
-})
+});
+
+$(document).ready(function(){
+    $(".icono-trailer").click(function(){
+        $(".columna__trailers").toggleClass("abierto");
+        $(".reproductor").toggleClass("abierto");
+    })});
